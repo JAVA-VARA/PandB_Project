15e9612add49c4e8df0ee16fee7d6351e1368b62
[sj.youns1027@gmail.com] <[sj.youns1027@gmail.com]>
2023-09-14 15:48:28 +0900

    [로그인 error 해결] 로그인 시 비밀번호 인증 오류 해결
    - signup.html: pw 확인란 삭제
    - login.html: form tag 수정(email -> username)
    
    RefreshToken entity와 DB 연동

--- src/main/java/sjspring/shop/pregAndBirthDeveloper/dto/CreateAccessTokenRequest.java
+++ src/main/java/sjspring/shop/pregAndBirthDeveloper/dto/CreateAccessTokenRequest.java
@@ -1,10 +1,12 @@
 package sjspring.shop.pregAndBirthDeveloper.dto;
 
 import lombok.Getter;
+//import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+
 public class CreateAccessTokenRequest {
     private String refreshToken;
 }
