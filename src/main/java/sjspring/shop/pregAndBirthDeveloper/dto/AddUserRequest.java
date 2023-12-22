4b4b7642367eebd1c97f359fcccff0b0a370efc3
[sj.youns1027@gmail.com] <[sj.youns1027@gmail.com]>
2023-12-22 22:29:21 +0900

    [CI] CI 실행 여부 확인

--- src/main/java/sjspring/shop/pregAndBirthDeveloper/dto/AddUserRequest.java
+++ src/main/java/sjspring/shop/pregAndBirthDeveloper/dto/AddUserRequest.java
@@ -4,7 +4,7 @@ package sjspring.shop.pregAndBirthDeveloper.dto;
 import lombok.*;
 import org.springframework.format.annotation.DateTimeFormat;
 
-import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 //사용자 정보를 담고 있는 객체
@@ -14,16 +14,16 @@ import javax.validation.constraints.Size;
 @NoArgsConstructor
 @AllArgsConstructor
 public class AddUserRequest {
-    @NotBlank(message="이메일을 입력해주세요.")
+    @NotNull(message="이메일을 입력해주세요.")
     private String email;
 
-    @NotBlank(message="이름을 입력해주세요.")
+    @NotNull(message="이름을 입력해주세요.")
     private String name;
 
-    @NotBlank(message = "비밀번호를 입력해주세요.")
+    @NotNull(message = "비밀번호를 입력해주세요.")
     private String password;
 
-    @NotBlank(message = "닉네임을 입력해주세요.")
+    @NotNull(message = "닉네임을 입력해주세요.")
     @Size(min = 2, max = 10, message = "닉네임은 2 ~ 10자 사이로 입력해주세요")
     private String nickName;
 
@@ -31,6 +31,4 @@ public class AddUserRequest {
 
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private String babyDue;
-
-
 }
