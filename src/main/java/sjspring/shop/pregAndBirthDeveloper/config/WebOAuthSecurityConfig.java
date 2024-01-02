eb2beb75733a2d13e1665f012ef1e3fa388ea8a1
[sj.youns1027@gmail.com] <[sj.youns1027@gmail.com]>
2024-01-02 16:24:50 +0900

    FILE UPLOAD 경로 수정 LOCAL -> AWS S3 BUCKET

--- src/main/java/sjspring/shop/pregAndBirthDeveloper/config/WebOAuthSecurityConfig.java
+++ src/main/java/sjspring/shop/pregAndBirthDeveloper/config/WebOAuthSecurityConfig.java
@@ -2,10 +2,19 @@ package sjspring.shop.pregAndBirthDeveloper.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import sjspring.shop.pregAndBirthDeveloper.config.jwt.TokenAuthenticationFilter;
@@ -16,17 +25,6 @@ import sjspring.shop.pregAndBirthDeveloper.config.oauth.OAuth2UserCustomService;
 import sjspring.shop.pregAndBirthDeveloper.repository.RefreshTokenRepository;
 import sjspring.shop.pregAndBirthDeveloper.service.UserDetailService;
 import sjspring.shop.pregAndBirthDeveloper.service.UserService;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.HttpStatusEntryPoint;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @RequiredArgsConstructor
 @Configuration
@@ -41,7 +39,7 @@ public class WebOAuthSecurityConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry){
         registry.addResourceHandler("/photo/**")
-                .addResourceLocations("file:///C:/Users/sjyou/IdeaProjects/PandBproject/PandB_Project/files/");
+                .addResourceLocations("s3://elasticbeanstalk-ap-northeast-2-944031405712/files/");
     }
 
 
